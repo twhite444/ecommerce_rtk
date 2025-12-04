1681959a9013400436269555f5efa462040e6bfc
Tom White <twhite4@bates.edu>
2025-12-04 13:30:06 -0500

    creating a global state log with redux

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,42 @@
-
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+    cartItems: [],
+};
 const CartSlice = ({
- 
+ name: 'cart',
+    initialState,
+    reducers: {
+        addItemToCart: (state, action) => {
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.cartItems.push({ ...action.payload, quantity: 1 });
+            }
+        },
+        removeItemFromCart: (state, action) => {
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload.id);
+        },
+        clearCart: (state) => {
+            state.cartItems = [];
+        },  
+        increaseItemQuantity: (state, action) => {
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload.id);
+            if (itemToIncrease) {
+                itemToIncrease.quantity += 1;
+            }
+        },
+        decreaseItemQuantity: (state, action) => {
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload.id);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+                itemToDecrease.quantity -= 1;
+            }
+        },
+
+    }
 });
+
+export const { addItemToCart, removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } = CartSlice.actions;
+export default CartSlice.reducer;
 
 
